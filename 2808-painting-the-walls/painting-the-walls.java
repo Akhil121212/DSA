267465d39a9ee7f6e63bb98d9f3e6fd649489901
Akhilesh Kumar C <cakhileshkumar123@gmail.com>
2026-08-18 22:38:05 +0530

    Time: 44 ms (50.73%) | Memory: 47.9 MB (23.30%) - LeetSync

--- 2808-painting-the-walls/painting-the-walls.java
+++ 2808-painting-the-walls/painting-the-walls.java
@@ -1,18 +1,28 @@
 class Solution {
-    public int paintWalls(int[] cost, int[] time) {
-        int n = cost.length; 
-        int dp[] = new int[n + 1]; 
-        Arrays.fill(dp, (int) 1e9); 
-        dp[0] = 0;
+    int[][] dp;
+    int solve(int i, int walls, int[] cost, int[] time) {   
+        if (walls <= 0)
+            return 0;
+        if (i == cost.length)
+            return 1000000000;
+        if (dp[i][walls] != -1)
+            return dp[i][walls];
 
-        
-        for (int i = 0; i < n; ++i) {
-            
-            for (int j = n; j > 0; --j) {
-               
-                dp[j] = Math.min(dp[j], dp[Math.max(j - time[i] - 1, 0)] + cost[i]);
-            }
-        }
-        return dp[n]; 
+        int paint = cost[i] + solve(i + 1, walls - 1 - time[i], cost, time);
+
+        int skip = solve(i + 1, walls, cost, time);
+
+        return dp[i][walls] = Math.min(paint, skip);
+    }
+
+    public int paintWalls(int[] cost, int[] time) {
+        int n = cost.length;
+
+        dp = new int[n][n + 1];
+
+        for (int[] row : dp)
+            Arrays.fill(row, -1);
+
+        return solve(0, n, cost, time);
     }
 }
