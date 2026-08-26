acd7d634150b173da19dc8691c04b7e792549e08
Akhilesh Kumar C <cakhileshkumar123@gmail.com>
2026-08-26 10:51:20 +0530

    Time: 84 ms (7.05%) | Memory: 158.5 MB (6.44%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,5 +1,6 @@
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.PriorityQueue;
 
 public class RuntimeHook {
 
@@ -16,23 +17,32 @@ public class RuntimeHook {
 }
 
 class Solution {
+
     public int[] maxSlidingWindow(int[] nums, int k) {
-        if (nums == null || nums.length == 0 || k <= 0) {
+
+        if (nums == null || nums.length == 0 || k <= 0 || k > nums.length) {
             return new int[0];
         }
 
         int n = nums.length;
         int[] result = new int[n - k + 1];
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[0] - a[0]);
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>(
+            (a, b) -> Integer.compare(b[0], a[0])
+        );
+
         int resultIndex = 0;
 
         for (int i = 0; i < n; i++) {
+
             pq.offer(new int[]{nums[i], i});
 
             if (i >= k - 1) {
+
                 while (pq.peek()[1] <= i - k) {
                     pq.poll();
                 }
+
                 result[resultIndex++] = pq.peek()[0];
             }
         }
